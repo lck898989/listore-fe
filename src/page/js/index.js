7e82cd237466241ddd7a9ed1efcc7bf1f624e7cd
lck898989 <1783887127@qq.com>
2017-10-04 13:04:55 +0800

    modify some file

--- src/page/js/index.js
+++ src/page/js/index.js
@@ -2,15 +2,16 @@
 * @Author: HP
 * @Date:   2017-10-03 09:34:06
 * @Last Modified by:   HP
-* @Last Modified time: 2017-10-03 15:57:26
+* @Last Modified time: 2017-10-04 11:36:28
 */
 /*document.write('it works.')
 //添加模块
 document.write(require('./module.js'))
 document.write('hello world')*/
 //加载jquery模块
-var m = require('jquery');
+var $$ = require('jquery');
 require('../css/index.css');
 require('../module.js');
 console.log("test");
-m('body').html('hello or sdfasd!');
+$$('body').html('hello javascript,hello java,c,c++,c#');
+
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,20 @@
 * @Author: HP
 * @Date:   2017-10-03 17:52:29
 * @Last Modified by:   HP
-* @Last Modified time: 2017-10-03 22:25:08
+* @Last Modified time: 2017-10-04 12:53:03
 */
 var webpack = require('webpack');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
+//引入html的插件
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+/*
+（第三种引入jquery的方法）
+var providePlugin = new webpack.ProvidePlugin({
+	$:'jquery',
+	jQuery:'jquery',
+	'window.jQuery':'jquery',
+	'window.$':'jquery',
+});*/
 var config = {
 	 //入口
       entry:{
@@ -18,6 +28,7 @@ var config = {
       	    filename:'js/[name].js'
       },
       externals:{
+      	//引入jquery的最佳方法（第二种引入js的方法）
       	'jquery':'window.jQuery'
 
       },
@@ -26,17 +37,32 @@ var config = {
                  {
                  	test:/\.css$/,
                  	loader:ExtractTextPlugin.extract("style-loader","css-loader")
-                 }
+                 },
+                /* 
+                (第三种引入jquery的方法)
+                {
+                 	test:require.resolve('jquery'), //此loader配置项的目标是NPM中的jquery
+                 	loader:'expose?$!expose?jQuery',//先把jQuery对象声明成为全局变量‘jQuery’,再通过管理进一步声明为全局变量‘$’
+                 }*/
       	]
       },
       plugins:[
-              //找到文件中的共有的一些东西
+              //找到文件中的共有的一些东西独立通用模块
               new webpack.optimize.CommonsChunkPlugin({
               	//找到entry中的common将其打包到js/base.js
               	name:'common',
               	filename:'js/base.js'
               }),
+              //提取css文件
               new ExtractTextPlugin('css/[name].css'),
+              /*new webpack.ProvidePlugin({
+									        '$$': 'jquery'
+									    }),*/ 
+			 //html模板的处理
+               new HtmlWebpackPlugin({
+					      title: 'My App',
+					      filename: 'assets/admin.html'
+					    })
       ]
 
 };
